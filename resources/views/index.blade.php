641a49d0d8b04b26279b1d6ee928d31a086b125a
ruirui38 <t.o.g.kendo0308@docomo.ne.jp>
2021-09-30 10:54:07 +0900

    bootstrap追加一覧画面変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>お店一覧</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
   <h1>お店一覧</h1>
 
   @foreach($shops as $shop)
@@ -14,5 +8,5 @@
     <p>{{ $shop->category->name }}</p>
     <p>{{ $shop->address }}</p>
   @endforeach
-</body>
-</html>
+  
+@endsection
